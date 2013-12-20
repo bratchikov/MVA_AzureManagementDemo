7e6d475d1d2961ac178acd166548158ed995db4e
Bratchikov Igor <bratchikov@ics.perm.ru>
2013-12-20 15:58:06 +0600

    Добавление демонстрационного веб-сайта.

---
+++ MVA_AzureManagementDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVA_AzureManagementDemo.Startup))]
+namespace MVA_AzureManagementDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
